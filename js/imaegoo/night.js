12a0117550a36e9fbaa8fcedaeb32910a8ac045a
eagle.guo <guoapeng@gmail.com>
2024-07-12 10:16:54 +0800

    Site updated: 2024-07-12 10:16:53

--- js/imaegoo/night.js
+++ js/imaegoo/night.js
@@ -3,7 +3,7 @@
    * Icarus 夜间模式 by iMaeGoo
    * https://www.imaegoo.com/
    */ 
-  var isNight = localStorage.getItem('night');
+  var isNight = localStorage.getItem('night') || true;
   var nightNav;
 
   function applyNight(value) {
